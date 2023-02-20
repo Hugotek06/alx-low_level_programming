6a135204aeb69c01b1ac5e890da0c63f5a22989b
hugotek06 <ugobaba06@gmail.com>
2023-02-19 20:06:26 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,46 @@
-Task 5
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main -Entry point
- * Description: prints allsingle digit numbers of
- * base 10 from 0
- * Return: Always 0 (success)
- */
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
- int a;
+ int c, i, k, j;
 
- for (a = 0; a < 10; a++)
+ for (c = 48; c <= 57; c++)
  {
-  printf("%d", a);
+  for (i = 48; i <= 57; i++)
+  {
+   for (k = 48; k <= 57; k++)
+   {
+    for (j = 48; j <= 57; j++)
+    {
+     if (((k + j) > (c + i) && k >= c) || c < k)
+     {
+      putchar(c);
+      putchar(i);
+      putchar(' ');
+      putchar(k);
+      putchar(j);
+
+     if (c + i + k + j == 227 && c == 57)
+     {
+     break;
+     }
+     else
+     {
+     putchar(',');
+     putchar(' ');
+     }
+     }
+    }
+   }
+  }
  }
+
  putchar('\n');
+
  return (0);
 }
