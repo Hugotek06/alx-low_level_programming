164c48a810ace97158aeb147b1f59e5570d368ca
hugotek06 <ugobaba06@gmail.com>
2023-02-21 19:40:09 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - print all alphabets
+ * @void
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+ int x;
+
+ for (x = 97; x < 123; x++)
+ {
+  putchar(x);
+ }
+ putchar('\n');
+}
