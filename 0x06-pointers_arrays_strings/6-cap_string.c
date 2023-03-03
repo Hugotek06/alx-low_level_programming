adff66173d6f4d06a2d69d73687bde7b0a8980fb
hugotek06 <ugobaba06@gmail.com>
2023-03-02 17:58:33 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,46 @@
-#include "main.h"
-
 /**
- * cap_string - capitalizes all words in a string
- * @s: string to capitalize
+ * cap_string - a function that capitalizes
+ * all words of a string
  *
- * Return: address of s
- */
+ * @s: pointer to char input array
+ *
+ * Return: @s
+*/
+
 char *cap_string(char *s)
 {
-int i = 0, j;
-char a[] = " \t\n,;.!?\"(){}";
+int i = 0;
 
-while (*(s + i))
+ /*iterate through our array values*/
+while (s[i] != '\0')
 {
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
+  /*check for any lowercase letters*/
+if (s[i] >= 97 && s[i] <= 122)
 {
+   /**
+    *
+    * if we have a null character
+    * change its value to capital
+   */
 if (i == 0)
-*(s + i) -= 'a' - 'A';
-else
 {
-for (j = 0; j <= 12; j++)
+s[i] -= 32;
+}
+   /**
+    * if we find any character matching the below before any small
+    * letter we change that value to a capital letter.
+   */
+if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+s[i - 1] == 124)
 {
-if (a[j] == *(s + i - 1))
-*(s + i) -= 'a' - 'A';
+s[i] -= 32;
 }
 }
-  }
-  i++;
+i++;
+ 
 }
- return (s);
+return (s);
 }
