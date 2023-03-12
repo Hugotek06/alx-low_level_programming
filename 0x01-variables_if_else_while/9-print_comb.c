ffa16cac9d246569fd449e0ee6a3574cfea0ac2b
hugotek06 <ugobaba06@gmail.com>
2023-03-12 09:40:26 -0700

    task 1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,24 +8,21 @@
  */
 int main(void)
 {
-
- int ascii;
-
- ascii = 48;
-
- while (ascii < 58)
-
- {
-  putchar(ascii);
-  if (ascii != 57)
-  {
-   putchar(44);
-   putchar(32);
-  } else
-  {
-   putchar('\n');
-  }
-  ascii++;
- }
- return (0);
+int ascii;
+ascii = 48;
+while (ascii < 58)
+{
+putchar(ascii);
+if (ascii != 57)
+{
+putchar(44);
+putchar(32);
+} 
+else
+{
+putchar('\n');
+}
+ascii++;
+}
+return (0);
 }
